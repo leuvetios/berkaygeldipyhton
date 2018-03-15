f2108e43cdd19595b44f1b7d4ddac90b81cb60c0
leuvetios <36761532+leuvetios@users.noreply.github.com>
2018-03-15 10:59:00 +0300

    week3

---
+++ week3/reverse.py
@@ -0,0 +1,9 @@
+str = input("Bir cümle ya da kelime giriniz :")
+index = len(str)
+new_str = []
+while index :
+    index -= 1
+    new_str.append(str[index])
+
+new2_str= ''.join(new_str)
+print(new2_str)
